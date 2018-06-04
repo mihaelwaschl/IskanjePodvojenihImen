b1955f52baebfa974ebf6d95e9521d0392ded180
Mihael <mihael.waschl@gmail.com>
2018-06-04 22:44:59 +0200

    Comitt 2

--- Iskanje Podvojenih Imen.py
+++ Iskanje Podvojenih Imen.py
@@ -3,18 +3,20 @@ import csv
 def read_file(csv_file):
     with open(csv_file, newline='', encoding= 'utf8' ) as csvfile:
         file = csv.reader(csvfile, delimiter=';')
-        #
-        # for row in file:
-        #     print(row)
-    return file
+        word = []
+        for row in file:
+            word.append(row[0])
+    return word
 
-def proces_one_line():
+def get_duplicated_names():
     while True:
         try:
             data = read_file(input('Vnesi ime datoteke!\n'))
+            for i in range(len(data)):
+                if data[i] == data[i-1]:
+                    print('Artikel {}, ki se nahaja na mestu {} je podvojen'.format(data[i], i+1) )
             break
         except FileNotFoundError:
             print('Datoteka ne obstaja ali ni v isti mapi, kot program! Poizkusi ponovno.')
-    print(data)
 
-proces_one_line()
+get_duplicated_names()
